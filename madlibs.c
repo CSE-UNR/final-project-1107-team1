08a2fae758daf8d4ed6e5907b8f1471c40de2e16
Louije Jan Sison <louijejansison@gmail.com>
2024-12-10 10:01:21 +0000

    Update madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -1,26 +1,73 @@
-//Authors: Louije Jan Sison, Arnav Jeyapandian, Yunling Shi
-
+//author:Louije Jan Sison, Yunling Shi
 #include <stdio.h>
-#include <stbool.h>
+#include <stdbool.h>
+#define yo 63
+#define ya 22
+#define big 1000
+#define filename "madlib1.txt"
+void count(int *k);
+int storef(FILE* ptr, int mac, char arr[][yo]);
+void displ(int mac, char arr[][yo]);
+//user input loop
+//swap
+//straighten string
+int main() {
+    FILE *file;
+   
+    int i, h, l;
+     count(&l);
+ h = l*2+1;
+char libi[h][yo], input[l][yo]; 
+    // Open the file for reading
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 0;
+    }
 
 
-int main(){
-FILE *readinput  
- readinput = fopen("madlib2.txt", "r");
+    i = storef(file, h, libi);
+     displ(i, libi);
+    
+     
 
+    // Close the file
+    fclose(file);
+    //write user input loop
+//swap user input and slice into main file
+    // Print the array
+    printf("(%s)\n", libi[5]);
+    printf("(%d)\n", l);
+    return 0;
+}
+void count(int *k){
+char madlib[ya][yo];
+int i, j;
+FILE *readinput;  
+readinput = fopen(filename, "r");
+for(i=0;i<ya;i++){
+for(j=0;j<ya;j++){
+fscanf(readinput,"%c", &madlib[i][j]);
+}
+}
+for(i=0;i<ya;i++){
+for(j=0;j<ya;j++){
+if(madlib[i][j-2] == 'A'){*k = *k + 1;} 
+if(madlib[i][j-2] == 'N'){*k = *k + 1;}  
+if(madlib[i][j-2] == 'V'){*k = *k + 1;}  
+}}
 
+ fclose(readinput);
+}
+int storef(FILE* ptr, int mac, char arr[][yo]){
+ptr = fopen(filename, "r");
+int y = 0;
+while(fgets(arr[y], yo, ptr) && y < mac){y++;}
+return y;
+ fclose(ptr);}
+void displ(int mac, char arr[][yo]){
 
-
-void swap1(char **str1_ptr, char **str2_ptr) 
-{ 
-char *temp = *str1_ptr; 
-*str1_ptr = *str2_ptr; 
-*str2_ptr = temp; 
-} 
-
-
-
-
-fclose(readinput);
-return 0;
+for(int y = 0; y<mac; y++){
+printf(" %s", arr[y]);
+}
 }

249cec43c4ea7f8e5ba38e34a6c0d689c0b95ab2
Louije Jan Sison <louijejansison@gmail.com>
2024-12-10 20:30:56 -0800

    Update madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -9,7 +9,7 @@ void acount(int *k);
 int storef(FILE* ptr, int mac, char arr[][yo]);
 void displ(int mac, char arr[][yo]);
 void uinput(FILE* fptr,int mac, char arr[][yo]);
-void swapstr(FILE* pnt, int mac, int mac2, char arr[][yo], char arr2[][yo]);
+void swapstr(FILE* pnt,char arr[][yo], char arr2[][yo], int inputsize);
 //straighten string
 int main() {
     FILE *file;
@@ -97,29 +97,32 @@ scanf("%s", arr[i]);
 }
 }
 }
-void swapstr(FILE* pnt, int mac, int mac2, char arr[][yo], char arr2[][yo]) {
-    char temp[100];
-    char buffer[mac][yo];
-int y, i;
-while(fgets(buffer[i], yo, pnt) && i < mac){i++;}
-for(y=0;y<mac2;y++){
-    if(buffer[y][yo] == 'A'){
-    for ( y = 0; arr[y] != '\0'; y++) {
-        temp[y] = arr[y];
-    }
-    temp[y] = '\0';
+void swapstr(char arr[][yo], char arr2[][yo], int inputsize) {
+    char temp[yo];
 
-    
-    for ( y = 0; arr2[y] != '\0'; y++) {
-        arr[y] = arr2[y];
-    }
-    arr[y] = '\0';
+    for (int i = 0; i < inputsize; i++) {
+        // Copy str1[i] to temp
+        int j = 0;
+        while (arr[i][j] != '\0') {
+            temp[j] = arr[i][j];
+            j++;
+        }
+        temp[j] = '\0';
 
-  
-    for ( y = 0; temp[y] != '\0'; y++) {
-        arr2[y] = temp[y];
+        // Copy str2[i] to str1[i]
+     
+        while (arr2[i][j] != '\0') {
+            arr[i][j] = arr2[i][j];
+            j++;
+        }
+        arr[i][j] = '\0';
+
+        // Copy temp to str2[i]
+
+        while (temp[j] != '\0') {
+            arr2[i][j] = temp[j];
+            j++;
+        }
+        arr2[i][j] = '\0';
     }
-    arr2[y] = '\0';
-    }
-}
 }
